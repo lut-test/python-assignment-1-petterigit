39acaaccd19ca26661428c7f13a6781a569367d5
petterigit <33552590+petterigit@users.noreply.github.com>
2021-03-04 18:21:40 +0200

    Fix returns for functions

--- assignment.py
+++ assignment.py
@@ -5,31 +5,28 @@
 def firstFive(word):
     # Hint - This line needs to be i = 5
     i = 5
-    print("The first five letters of your word are: " + word[:i])
+    return word[:i]
 
 
 def lastFive(word):
     # For this one, you are going to need to keep the len(word)
     i = len(word) - 5
-    print("The five last letters are: " + word[i:])
+    return word[i:]
 
 
 def middleFour(word):
     i = 1
     n = 5
-    print("Letters 2, 3, 4, and 5 are: " + word[i:n])
+    return word[i:n]
 
 
 def everyOther(word):
     i = 1
     n = 2
 
-    print(
-        "Every other letter of the word, starting from the second letter are: "
-        + word[i::n]
-    )
+    return word[i::n]
 
 
 def backwards(word):
     i = -1
-    print("Your word written backwards is: " + word[::i])
+    return word[::i]
--- assignmentMain.py
+++ assignmentMain.py
@@ -3,11 +3,14 @@ import assignment
 
 def main():
     word = input("Give a word: ")
-    assignment.firstFive(word)
-    assignment.lastFive(word)
-    assignment.middleFour(word)
-    assignment.everyOther(word)
-    assignment.backwards(word)
+    print("The first five letters of your word are: " + assignment.firstFive(word))
+    print("The five last letters are: " + assignment.lastFive(word))
+    print("Letters 2, 3, 4, and 5 are: " + assignment.middleFour(word))
+    print(
+        "Every other letter of the word, starting from the second letter are: "
+        + assignment.everyOther(word)
+    )
+    print("Your word written backwards is: " + assignment.backwards(word))
 
 
 main()
